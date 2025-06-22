8a2ecb3a07196b984704ca384b758d1be1f9b98c
Metalbenji <64557500+Metalbenji@users.noreply.github.com>
2025-06-22 06:51:28 +0100

    Add files via upload

--- functions/get-user-data.js
+++ functions/get-user-data.js
@@ -5,8 +5,6 @@ exports.handler = async (event, context) => {
   try {
     console.log("Function started");
     
-    // Since the data folder is in the same functions directory,
-    // we can reference it directly
     const dataPath = path.join(__dirname, 'data', 'achievements.json');
     console.log("Data file path:", dataPath);
     
@@ -15,6 +13,16 @@ exports.handler = async (event, context) => {
     
     const data = JSON.parse(rawData);
     console.log("Data parsed successfully, entries:", Object.keys(data).length);
+    
+    // Debug: Log all usernames in the data
+    const usernames = Object.keys(data);
+    console.log("All usernames in data:", usernames);
+    
+    // Debug: Log a sample entry structure
+    if (usernames.length > 0) {
+      const firstUser = usernames[0];
+      console.log("Sample user structure:", firstUser, "->", JSON.stringify(data[firstUser], null, 2));
+    }
     
     return {
       statusCode: 200,
